45d5415d61a095dd9508432b2e6b6072c3d4a48e
Heelin Mistry <hmistry69@gmail.com>
2026-06-11 10:08:00 +0200

    Update MatchDetailUseCasesProvider.swift

--- community_app/community_app/Frameworks/CommunityCore/Source/UseCaseProviders/MatchDetailUseCasesProvider.swift
+++ community_app/community_app/Frameworks/CommunityCore/Source/UseCaseProviders/MatchDetailUseCasesProvider.swift
@@ -6,13 +6,13 @@
 //
 
 /// A provider protocol for accessing match-related and notification-related use cases.
-public protocol MatchDetailUseCasesProvider {
+public protocol MatchDetailUseCasesProvider: Sendable {
     
     /// Provides access to match-specific use case operations.
     var matches: MatchUseCaseProtocol { get }
     /// Provides access to notification-specific use case operations.
-    var notifications: NotificationProtocol { get }
+    @MainActor var notifications: NotificationProtocol { get }
     /// Provides access to location-specific use case operations.
-    var location: LocationProtocol { get }
+    @MainActor var location: LocationProtocol { get }
     
 }
